2a02e5232d3598e8b3099a8b3fc42b07aeb66710
jglrxavpok <jglrxavpok@gmail.com>
2017-05-09 20:17:34 +0200

    Mini changement

--- src/main/java/fr/minecraftforgefrance/tutorial/client/ClientEventHandler.java
+++ src/main/java/fr/minecraftforgefrance/tutorial/client/ClientEventHandler.java
@@ -1,5 +1,6 @@
 package fr.minecraftforgefrance.tutorial.client;
 
+import net.minecraft.client.gui.ScaledResolution;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -8,8 +9,12 @@ public class ClientEventHandler {
     @SubscribeEvent
     public void onGuiDrawing(RenderGameOverlayEvent.Pre event) {
         if(event.getType() == RenderGameOverlayEvent.ElementType.HOTBAR) {
-            Renderer.drawTransparentSprite(event.getResolution());
-            Renderer.drawModel(event.getResolution());
+            draw(event.getResolution());
         }
+    }
+
+    private void draw(ScaledResolution resolution) {
+        Renderer.drawTransparentSprite(resolution);
+        Renderer.drawModel(resolution);
     }
 }
