a6398a3f393731edae1a95428ec7c38a3782878f
jglrxavpok <jglrxavpok@gmail.com>
2017-05-10 19:36:19 +0200

    Ajout des éléments pour glColor

--- src/main/java/fr/minecraftforgefrance/tutorial/client/ClientEventHandler.java
+++ src/main/java/fr/minecraftforgefrance/tutorial/client/ClientEventHandler.java
@@ -4,6 +4,8 @@ import net.minecraft.client.gui.ScaledResolution;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import static org.lwjgl.opengl.GL11.*;
+
 public class ClientEventHandler {
 
     @SubscribeEvent
@@ -17,7 +19,20 @@ public class ClientEventHandler {
      * C'est la méthode que l'on va modifier dans ce tutoriel
      */
     private void draw(ScaledResolution resolution) {
-        Renderer.drawTransparentSprite(resolution);
-        Renderer.drawModel(resolution);
+        glColor3f(0f, 0f, 1f);
+        Renderer.drawTransparentSprite(resolution); // rendu du logo du site avec uniquement le bleu
+
+        glPushMatrix();
+        glTranslatef(100f, 0f, 0f); // juste pour tout voir sur l'écran en même temps
+
+        glColor4f(0.5f, 0.5f, 0f, 0.25f);
+        Renderer.drawTransparentSprite(resolution); // rendu du logo du site à 25% d'opacité, pas de bleu, et division par 2 du rouge et du bleu
+
+        glTranslatef(100f, 0f, 0f); // juste pour tout voir sur l'écran en même temps
+
+        glColor3f(0f, 0f, 0f);
+        glColor4f(1f, 1f, 1f, 1f);
+        Renderer.drawTransparentSprite(resolution); // rendu du logo du site à 25% d'opacité, pas de bleu, et division par 2 du rouge et du bleu
+        glPopMatrix();
     }
 }
