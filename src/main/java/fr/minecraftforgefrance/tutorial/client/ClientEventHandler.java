04c69a4b5f74180daacdc0f7751f6ceea21f4fe2
jglrxavpok <jglrxavpok@gmail.com>
2017-05-20 20:19:25 +0200

    Ajout de la partie sur le scissor test

--- src/main/java/fr/minecraftforgefrance/tutorial/client/ClientEventHandler.java
+++ src/main/java/fr/minecraftforgefrance/tutorial/client/ClientEventHandler.java
@@ -1,8 +1,10 @@
 package fr.minecraftforgefrance.tutorial.client;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import static org.lwjgl.opengl.GL11.*;
 
 public class ClientEventHandler {
 
@@ -17,7 +19,9 @@ public class ClientEventHandler {
      * C'est la méthode que l'on va modifier dans ce tutoriel
      */
     private void draw(ScaledResolution resolution) {
-        Renderer.drawTransparentSprite(resolution);
-        Renderer.drawModel(resolution);
+        glEnable(GL_SCISSOR_TEST);
+        glScissor(32,Minecraft.getMinecraft().displayHeight-128, 64,64); // attention! Pour l'axe Y, le 0 est en **bas** de l'écran!
+        Renderer.drawOpaqueSprite(resolution);
+        glDisable(GL_SCISSOR_TEST);
     }
 }
